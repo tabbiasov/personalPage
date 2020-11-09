77b3e2ceb5ced8e419b7f4137b4e951cf38e4460
Timur Abbiasov <abbyasov@gmail.com>
2020-11-09 00:21:58 -0500

    Updates

--- component---src-pages-404-js-e653d61ddb40b69bb790.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("L12J"),c=n("vrFN");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("h1",null," No such page exists, yet ")," ")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-e653d61ddb40b69bb790.js.map
